eea0c6b34b66bfef6f786adc0fe6e7ab49d3a90b
Madison Kriege <46975132+madison-kriege@users.noreply.github.com>
2024-01-07 11:55:16 -0600

    Added 167 Styling

--- app/src/main/java/com/example/cotcscoutingapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/cotcscoutingapp/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.example.cotcscoutingapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Green80 = Color(0xFF4CAF50)
+val GreenGray80 = Color(0xFFB1CEA4)
+val Yellow80 = Color(0xFFF5E472)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Green40 = Color(0xFF8BC34A)
+val GreenGray40 = Color(0xFF66715B)
+val Yellow40 = Color(0xFFFFEB3B)
