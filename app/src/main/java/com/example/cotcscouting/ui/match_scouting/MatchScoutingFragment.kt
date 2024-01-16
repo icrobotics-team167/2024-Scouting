0ac8327f2d70380eafb8a1b48eb81e7a0c082282
Madison Kriege <madison.kriege@pearson.com>
2024-01-16 10:49:27 -0600

    Calder 1/16 Button Examples

--- app/src/main/java/com/example/cotcscouting/ui/match_scouting/MatchScoutingFragment.kt
+++ app/src/main/java/com/example/cotcscouting/ui/match_scouting/MatchScoutingFragment.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.cotcscouting.databinding.FragmentMatchScoutingBinding
+import com.example.cotcscouting.ui.pit_scouting.PitScoutingViewModel
 
-class MatchScoutingFragment : Fragment() {
+class MatchScoutingFragment : Fragment()  {
 
     private var _binding: FragmentMatchScoutingBinding? = null
 
@@ -20,8 +23,59 @@ class MatchScoutingFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        val matchScoutingViewModal =
+            ViewModelProvider(this)[MatchScoutingViewModel::class.java]
+
         _binding = FragmentMatchScoutingBinding.inflate(inflater, container, false)
-        return binding.root
+        val root: View = binding.root
+
+        val textView: TextView? = binding.textDashboard
+        matchScoutingViewModal.text.observe(viewLifecycleOwner) {
+            if (textView != null) {
+                textView.text = it
+            }
+        }
+
+        var autoAmpCount = 0
+        binding.autoAmpDec?.setOnClickListener {
+            if(autoAmpCount > 0) {
+                autoAmpCount--
+            }
+            binding.ampNoteAuto?.text = autoAmpCount.toString()
+        }
+
+        binding.autoAmpInc?.setOnClickListener {
+            autoAmpCount++
+            binding.ampNoteAuto?.text = autoAmpCount.toString()
+        }
+
+        var autoSpeakerCount = 0
+        binding.autoSpeakerDec?.setOnClickListener {
+            if(autoSpeakerCount > 0) {
+                autoSpeakerCount--
+            }
+            binding.speakerNoteAuto?.text = autoSpeakerCount.toString()
+        }
+
+        binding.autoSpeakerInc?.setOnClickListener {
+            autoSpeakerCount++
+            binding.speakerNoteAuto?.text = autoSpeakerCount.toString()
+        }
+
+        var teleopAmpCount = 0
+        binding.teleOpAmpDec?.setOnClickListener {
+            if(teleopAmpCount > 0) {
+                teleopAmpCount--
+            }
+            binding.ampNoteTeleOp?.text = teleopAmpCount.toString()
+        }
+
+        binding.teleOpAmpInc?.setOnClickListener {
+            teleopAmpCount++
+            binding.ampNoteTeleOp?.text = teleopAmpCount.toString()
+        }
+
+        return root
     }
 
     override fun onDestroyView() {
