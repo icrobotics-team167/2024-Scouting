7e839b3be0752732b8f51d84e8a1722cbae06e9c
Madison Kriege <46975132+madison-kriege@users.noreply.github.com>
2024-02-12 18:49:32 -0600

    EVERYTHING IS AWESOME!!!!! YEEWY! -C

--- app/src/main/java/com/example/cotcscouting/MainActivity.kt
+++ app/src/main/java/com/example/cotcscouting/MainActivity.kt
@@ -35,5 +35,6 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+        LoginFragment().show(supportFragmentManager, "login_fragment")
     }
 }
