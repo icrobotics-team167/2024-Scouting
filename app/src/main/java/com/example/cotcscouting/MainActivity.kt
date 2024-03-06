7b2e8c8fd7a9f79b442ed7456364596c339a77f6
Madison Kriege <madison.kriege@pearson.com>
2024-03-05 19:21:28 -0600

    Switch to shared preferences

--- app/src/main/java/com/example/cotcscouting/MainActivity.kt
+++ app/src/main/java/com/example/cotcscouting/MainActivity.kt
@@ -6,10 +6,10 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.cotcscouting.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -32,7 +32,7 @@ class MainActivity : AppCompatActivity() {
             )
         )
         navView.setupWithNavController(navController)
-        LoginFragment().show(supportFragmentManager, "login_fragment")
+            LoginFragment().show(supportFragmentManager, "login_fragment")
     }
 
 }
