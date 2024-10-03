1cfa2b85f29af6c276d51d46a0430b5db31e2ae5
Syatram <157168084+Syatram@users.noreply.github.com>
2024-10-03 18:28:25 -0500

    Added a two functions for creating options.  - createOption(name : String, row : TableRow)   - Plan to flesh it out to make it to edit the button more or just remove the function and put the code in the create options function   - createOptions(names : Array<String>, layout : TableLayout)    - I have to go make mashed potatoes so I can't finish the function.    - The plan for the function is to divide the options into two.    - For every button there should be data which will be shown when clicked    - Currently mostly just to test if I knew how to make ui objects.
      TODO:
      - Make a table to the right of buttons which displays data.
      - Make a thing that graphs data next to the table that displays data.
       - Ideally, there would be a scroll frame which snaps between each.
      - Do more when I finish making mashed potatoes.

--- app/src/main/java/com/example/cotcscouting/ui/graphing_testing/GraphingTestingFragment.kt
+++ app/src/main/java/com/example/cotcscouting/ui/graphing_testing/GraphingTestingFragment.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TableLayout
+import android.widget.TableRow
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.cotcscouting.databinding.FragmentGraphingTestingBinding
+import kotlin.math.ceil
+import kotlin.math.roundToInt
 
 class GraphingTestingFragment : Fragment() {
     private var _binding: FragmentGraphingTestingBinding? = null
@@ -32,10 +37,31 @@ class GraphingTestingFragment : Fragment() {
                 textView.text = it
             }
         }
+        val list = arrayOf("B1", "B2", "B3")
+        createOptions(list, binding.tableLayout!!) // Might want to get rid of the assertion idk
         return binding.root
     }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    // Won't work with odds but rn I just want to test that I know how to create buttons.
+    fun createOptions(names : Array<String>, layout : TableLayout) {
+        val max = ceil(names.size/2.0).roundToInt()
+        println(names.size)
+        println(max)
+
+        for(element in names) {
+            val tableRow = TableRow(this.context)
+            createOption(element, tableRow)
+            layout.addView(tableRow)
+        }
+    }
+
+    fun createOption(name : String, row : TableRow) {
+        val button = Button(this.context)
+        button.text = name
+        row.addView(button)
     }
 }
